4027c5d7457fed3fb3bfe272176a42a0937d7daf
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-10-12 10:23:15 -0300

    add background component

--- mobile/src/pages/Auth/SignIn/index.js
+++ mobile/src/pages/Auth/SignIn/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-// import { Container } from './styles';
+import Background from '~/components/Background';
+
+import { Container } from '../styles';
 
 export default function SignIn() {
-  return <Text>SignIn</Text>;
+  return (
+    <Background>
+      <Container>
+        <Text>SignIn</Text>
+      </Container>
+    </Background>
+  );
 }
